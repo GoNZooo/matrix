68ff02695b0b91ac2037961a7c46602d1ac3ada1
Rickard Andersson <gonz@severnatazvezda.com>
2015-12-08 01:58:26 +0200

    Reindent remind.rkt

--- src/remind.rkt
+++ src/remind.rkt
@@ -33,35 +33,35 @@
 (provide db/get/reminders)
 (define (db/get/reminders)
   (call-with-transaction
-    db-conn
-    (lambda ()
-      (query-rows
-        db-conn
-        (string-append "SELECT *,rowid FROM reminders WHERE datetime LIKE "
-                       "strftime('%Y-%m-%d %H:%M:00', 'now', 'localtime')")))))
+   db-conn
+   (lambda ()
+     (query-rows
+      db-conn
+      (string-append "SELECT *,rowid FROM reminders WHERE datetime LIKE "
+                     "strftime('%Y-%m-%d %H:%M:00', 'now', 'localtime')")))))
 
 (provide db/add/reminder)
 (define (db/add/reminder user place datetime text)
   (call-with-transaction
-    db-conn
-    (lambda ()
-      (query-exec
-        db-conn
-        "INSERT INTO reminders values ($1, $2, $3, $4)"
-        user place (bg-time->sql-time datetime) text))
-    #:option 'immediate
-    #:isolation 'read-uncommitted))
+   db-conn
+   (lambda ()
+     (query-exec
+      db-conn
+      "INSERT INTO reminders values ($1, $2, $3, $4)"
+      user place (bg-time->sql-time datetime) text))
+   #:option 'immediate
+   #:isolation 'read-uncommitted))
 
 (define (db/remove/reminder reminder-id)
   (call-with-transaction
-    db-conn
-    (lambda ()
-      (query-exec
-        db-conn
-        "DELETE FROM reminders WHERE rowid = $1"
-        (reminder/id reminder-id)))
-    #:option 'immediate
-    #:isolation 'read-uncommitted))
+   db-conn
+   (lambda ()
+     (query-exec
+      db-conn
+      "DELETE FROM reminders WHERE rowid = $1"
+      (reminder/id reminder-id)))
+   #:option 'immediate
+   #:isolation 'read-uncommitted))
 
 (provide db/remove/reminders)
 (define (db/remove/reminders reminder-ids)
@@ -72,10 +72,10 @@
   (define room-id
     (if (equal? (reminder/place reminder)
                 "priv")
-      (hash-ref (room/create/private `(,(reminder/user reminder))
-                                     "api-bot reminder")
-                'room_id)
-      (reminder/place reminder)))
+        (hash-ref (room/create/private `(,(reminder/user reminder))
+                                       "api-bot reminder")
+                  'room_id)
+        (reminder/place reminder)))
 
   (sleep 2)
   (send/room/message/id room-id
@@ -100,25 +100,25 @@
                      datetime-regex)))
   (if (regexp-match? command-regex
                      message-content)
-    (let*-values
-      ([(extraction-regex)
-        (pregexp (format ".remind (~a) (~a) (~a) (.*)"
-                         user-regex
-                         place-regex
-                         datetime-regex))]
-       [(user place datetime text)
-        (with-matches extraction-regex
-                      message-content
-                      (values (m 1) (m 2) (m 3) (m 4)))])
-      (define param-user (if (equal? user "me")
-                           (event/user-id message)
-                           user))
-      (define param-place
-        (if (equal? place "here")
-          (event/room-id message)
-          "priv"))
+      (let*-values
+          ([(extraction-regex)
+            (pregexp (format ".remind (~a) (~a) (~a) (.*)"
+                             user-regex
+                             place-regex
+                             datetime-regex))]
+           [(user place datetime text)
+            (with-matches extraction-regex
+              message-content
+              (values (m 1) (m 2) (m 3) (m 4)))])
+        (define param-user (if (equal? user "me")
+                               (event/user-id message)
+                               user))
+        (define param-place
+          (if (equal? place "here")
+              (event/room-id message)
+              "priv"))
 
-      (define param-datetime (parse/datetime/absolute/bg datetime))
-      (values param-user param-place param-datetime text))
-    (values #f #f #f #f)))
+        (define param-datetime (parse/datetime/absolute/bg datetime))
+        (values param-user param-place param-datetime text))
+      (values #f #f #f #f)))
 
